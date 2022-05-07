075735ea2aa7d601ee777bc9d3b906e0e04bde75
LaitStorm <joseulloae@uc.cl>
2022-05-06 21:37:49 -0400

    prueba

--- js/interaccion.js
+++ js/interaccion.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     chil.mouseenter(function(){
         var nomreg = $(this).attr("name");
         $("#tit").text(nomreg);
-        $(this).fadeTo("fast",0);
+        $(this).fadeTo("fast",0.5);
         console.log("no");
     });
 
