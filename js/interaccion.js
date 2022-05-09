96e802d17d073174055a147acf11b9a739e0f953
LaitStorm <joseulloae@uc.cl>
2022-05-09 01:52:08 -0400

    logo+ventanas

--- js/interaccion.js
+++ js/interaccion.js
@@ -1,16 +1,48 @@
 $(document).ready(function(){ 
     var chil = $(".chilito");
     chil.mouseenter(function(){
-        var nomreg = $(this).attr("name");
-        $("#tit").text(nomreg);
         $(this).fadeTo("fast",0.5);
         console.log("no");
     });
 
     chil.mouseleave(function(){
         $(this).fadeTo("fast",1);
-        $("#tit").text("Región");
         console.log("tampoco");
     });
+
+    $(".Parrafos").hide()
+
+    var nort = $("#Norte");
+    var su = $("#Sur");
+
+    nort.mouseenter(function(){
+        $("#Nort").show();
+        console.log("hehe");
+    });
+    nort.mouseleave(function(){
+        $("#Nort").hide();
+        console.log("hehe2");
+    });
+
+    su.mouseenter(function(){
+        $("#Su").show();
+        console.log("hehe33");
+    });
+    su.mouseleave(function(){
+        $("#Su").hide();
+        console.log("hehe3");
+    });
+
+    var cent = $("#Centro");
+    cent.mouseenter(function(){
+        $("#Cent").show();
+        console.log("hehe");
+    });
+    cent.mouseleave(function(){
+        $("#Cent").hide();
+        console.log("hehe2");
+    });
 });
+
+
 
