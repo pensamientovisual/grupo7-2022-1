808e390cf73a92f4842cbb44e9bb6d649dc83191
LaitStorm <joseulloae@uc.cl>
2022-05-09 23:52:21 -0400

    ventanas reg

--- js/interaccion.js
+++ js/interaccion.js
@@ -12,34 +12,154 @@ $(document).ready(function(){
 
     $(".Parrafos").hide()
 
-    var nort = $("#Norte");
-    var su = $("#Sur");
-
-    nort.mouseenter(function(){
-        $("#Nort").show();
+    
+    var ay = $("#CHL2691");
+    ay.mouseenter(function(){
+        $("#TAY").show();
         console.log("hehe");
     });
-    nort.mouseleave(function(){
-        $("#Nort").hide();
+    ay.mouseleave(function(){
+        $("#TAY").hide();
         console.log("hehe2");
     });
 
-    su.mouseenter(function(){
-        $("#Su").show();
-        console.log("hehe33");
-    });
-    su.mouseleave(function(){
-        $("#Su").hide();
-        console.log("hehe3");
-    });
-
-    var cent = $("#Centro");
-    cent.mouseenter(function(){
-        $("#Cent").show();
+    var ma = $("#CHL2692");
+    ma.mouseenter(function(){
+        $("#TMA").show();
         console.log("hehe");
     });
-    cent.mouseleave(function(){
-        $("#Cent").hide();
+    ma.mouseleave(function(){
+        $("#TMA").hide();
+        console.log("hehe2");
+    });
+
+    var ta = $("#CHL2693");
+    ta.mouseenter(function(){
+        $("#TTA").show();
+        console.log("hehe");
+    });
+    ta.mouseleave(function(){
+        $("#TTA").hide();
+        console.log("hehe2");
+    });
+
+    var ar = $("#CHL2694");
+    ar.mouseenter(function(){
+        $("#TAR").show();
+        console.log("hehe");
+    });
+    ar.mouseleave(function(){
+        $("#TAR").hide();
+        console.log("hehe2");
+    });
+
+    var ant = $("#CHL2695");
+    ant.mouseenter(function(){
+        $("#TANT").show();
+        console.log("hehe");
+    });
+    ant.mouseleave(function(){
+        $("#TANT").hide();
+        console.log("hehe2");
+    });
+
+    var atac = $("#CHL2696");
+    atac.mouseenter(function(){
+        $("#TATAC").show();
+        console.log("hehe");
+    });
+    atac.mouseleave(function(){
+        $("#TATAC").hide();
+        console.log("hehe2");
+    });
+
+    var coq = $("#CHL2697");
+    coq.mouseenter(function(){
+        $("#TCOQ").show();
+        console.log("hehe");
+    });
+    coq.mouseleave(function(){
+        $("#TCOQ").hide();
+        console.log("hehe2");
+    });
+
+    var rm = $("#CHL2698");
+    rm.mouseenter(function(){
+        $("#TRM").show();
+        console.log("hehe");
+    });
+    rm.mouseleave(function(){
+        $("#TRM").hide();
+        console.log("hehe2");
+    });
+
+    var val = $("#CHL2699");
+    val.mouseenter(function(){
+        $("#TVAL").show();
+        console.log("hehe");
+    });
+    val.mouseleave(function(){
+        $("#TVAL").hide();
+        console.log("hehe2");
+    });
+
+    var la = $("#CHL2700");
+    la.mouseenter(function(){
+        $("#TLA").show();
+        console.log("hehe");
+    });
+    la.mouseleave(function(){
+        $("#TLA").hide();
+        console.log("hehe2");
+    });
+
+    var ri = $("#CHL2701");
+    ri.mouseenter(function(){
+        $("#TRI").show();
+        console.log("hehe");
+    });
+    ri.mouseleave(function(){
+        $("#TRI").hide();
+        console.log("hehe2");
+    });
+
+    var bi = $("#CHL2702");
+    bi.mouseenter(function(){
+        $("#TBI").show();
+        console.log("hehe");
+    });
+    bi.mouseleave(function(){
+        $("#TBI").hide();
+        console.log("hehe2");
+    });
+
+    var bo = $("#CHL2703");
+    bo.mouseenter(function(){
+        $("#TBO").show();
+        console.log("hehe");
+    });
+    bo.mouseleave(function(){
+        $("#TBO").hide();
+        console.log("hehe2");
+    });
+
+    var ll = $("#CHL2704");
+    ll.mouseenter(function(){
+        $("#TLL").show();
+        console.log("hehe");
+    });
+    ll.mouseleave(function(){
+        $("#TLL").hide();
+        console.log("hehe2");
+    });
+
+    var maul = $("#CHL2705");
+    maul.mouseenter(function(){
+        $("#TMAUL").show();
+        console.log("hehe");
+    });
+    maul.mouseleave(function(){
+        $("#TMAUL").hide();
         console.log("hehe2");
     });
 });
